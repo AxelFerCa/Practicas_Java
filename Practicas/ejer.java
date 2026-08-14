a5641ff4aa0cfeaf0aebf2827b803755af019153
AxelFerCa <axelferca.py@gmail.com>
2026-08-14 11:38:35 -0600

    Primer Hola mundo

---
+++ Practicas/ejer.java
@@ -0,0 +1,8 @@
+public class ejer {
+    public static void main(String[] args) {
+
+        System.out.println("hola mundo soy axel");
+        System.out.println("Estoy aprendiendo Java");
+
+    }
+}
