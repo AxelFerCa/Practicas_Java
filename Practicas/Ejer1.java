423ff74269b3a9670d621b6bda8e1d4acae2dd2f
AxelFerCa <axelferca.py@gmail.com>
2026-08-19 03:48:24 -0600

    if y else en java

---
+++ Practicas/Ejer1.java
@@ -0,0 +1,15 @@
+public class Ejer1 {
+    public static void main(String [] args) {
+    //esto es importante porque String nunca cambia aunque trabajes con enteros, numeros con decimales, etc 
+        int nivel = 18;
+
+        if (nivel >= 18){
+            System.out.println("eres crack");
+        }else{
+
+            System.out.println("no eres crack :(");
+        }
+    }
+}
+
+// ejecucion de if y else en java 
